0872bc198932f02b87309542af0ecb2dd3a92b80
Elliot Mackinnon <elliot.mackinnon@techtonic.com>
2021-02-27 11:27:19 -0700

    Return chat history on join event

--- server/server.js
+++ server/server.js
@@ -7,11 +7,31 @@ const io = require('socket.io')(http, {
 });
 const { Message, User } = require('./bookshelf');
 
-io.on('connection', (socket) => {
-  socket.on('message', async ({ user, message }) => {
-    // const user = await new User({ name }).save();
-    // const msg = await new Message({ message, user_id: user.get('id') }).save();
-    io.emit('message', { user, message });
+const findOrCreateUser = async (userName) => {
+  let user;
+  user = await User.where({ name: userName }).fetch({ require: false });
+  if (!user) {
+    const newUser = await new User({ name: userName }).save();
+    user = newUser;
+  }
+  return user;
+};
+
+const returnChatHistory = async () => {
+  return await Message.fetchAll({ require: false });
+};
+
+io.on('connection', async (socket) => {
+  const history = await returnChatHistory();
+  socket.emit('join', { chat: history });
+
+  socket.on('messageSent', async ({ user, message }) => {
+    const sender = await findOrCreateUser(user);
+    console.log(sender.get('id'));
+
+    await new Message({ message, user_id: sender.get('id'), sender: user }).save();
+
+    io.emit('messageSent', { chat: history });
   });
 });
 
